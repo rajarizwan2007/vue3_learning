e98f7b400a597c82633c0f18c934854d7e81a3ee
Raja Rizwan <rajarizwan2007@gmail.com>
2022-09-26 23:10:28 +0500

    task tracker app first commit, includes two of basic commponts i.e headerComponent and ButtonComponent

---
+++ task_tracker_app/src/components/ButtonComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <button class="btn" @click="onClick()" :style="{background:color}">{{text}}</button>
+</template>
+<script>
+    export default{
+        name:'ButtonComponent',
+        props:{
+            text: String,
+            color: String
+        },
+        methods:{
+            onClick(){
+                console.log("its working")
+            }
+        }
+    }
+</script>
---
+++ task_tracker_app/src/components/HeaderComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <ButtonComponent text="Add Task" color="blue"/>
+    </header>
+</template>
+
+<script>
+import ButtonComponent from "./ButtonComponent.vue"
+
+    export default{
+        name: 'HeaderComponent',
+        props: {
+            title: String,
+        },
+        components:{
+            ButtonComponent,
+}
+    }
+</script>
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
